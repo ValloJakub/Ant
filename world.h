185908e3ee01eb16f9bb60fa9a484dcec95d4bb8
mravec39 <mravec39@stud.uniza.sk>
2024-01-08 18:54:27 +0100

    world.c a world.h update

--- world.h
+++ world.h
@@ -7,27 +7,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-#define MAX_ANTS 5  // Počet mravcov
-#define STEPS_LIMIT 50 // Počet krokov do konca hry
-
-typedef struct world {
+typedef struct World {
+    int stepLimit;
+    int max_ants;
     int size;
     char **grid;
-    pthread_mutex_t mutex;
-    Ant ants[MAX_ANTS];
 } World;
 
 void moveAnt(Ant *ant, World *world);
-void initializeWorld(World *world, int size);
+
 void initializeRandomBlackCells(World *world, int num_black_cells);
-void initializeRandomAnts(World *world);
+void initializeWorld(World *world, int size, int max_ants);
+void initializeRandomAnts(Ant ants[], World *world, pthread_mutex_t *mutex_ant);
+
 void displayWorld(World *world);
+void setBlackCellsManually(World *world, int x, int y);
+
 void freeWorldMemory(World *world);
-
-void setBlackCell(World *world, int x, int y);
-
-
 
 void saveWorldToFile(World *world, const char *filename);
 void loadWorldFromFile(World *world, const char *filename);
